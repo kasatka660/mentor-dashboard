693a7626677b0b55d5fbe0a82d1c43828f1f682c
Elena Khlyavskaya <lenach241@gmail.com>
2019-02-24 23:46:49 +0300

    update build

---
+++ mentor-dashboard/build/precache-manifest.a5920b7172ace34496dca9bb3fb55317.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "47c492395f8cfcced7f5",
+    "url": "/mentor-dashboard/mentor-dashboard/build/static/js/runtime~main.47c49239.js"
+  },
+  {
+    "revision": "5dae5dfa4161d0d0832a",
+    "url": "/mentor-dashboard/mentor-dashboard/build/static/js/main.5dae5dfa.chunk.js"
+  },
+  {
+    "revision": "b2d15a7841991329bd39",
+    "url": "/mentor-dashboard/mentor-dashboard/build/static/js/1.b2d15a78.chunk.js"
+  },
+  {
+    "revision": "5dae5dfa4161d0d0832a",
+    "url": "/mentor-dashboard/mentor-dashboard/build/static/css/main.44ed56d6.chunk.css"
+  },
+  {
+    "revision": "a51bb34a10f678da28257567c9b2f180",
+    "url": "/mentor-dashboard/mentor-dashboard/build/index.html"
+  }
+];
