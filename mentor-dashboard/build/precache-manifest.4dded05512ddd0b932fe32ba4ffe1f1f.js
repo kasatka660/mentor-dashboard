1887f5e2e744dbbe051608cfd834cdf1f6e1c6b1
Elena Khlyavskaya <lenach241@gmail.com>
2019-02-24 22:49:48 +0300

    gh-pages build

---
+++ mentor-dashboard/build/precache-manifest.4dded05512ddd0b932fe32ba4ffe1f1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24395b9ac66cfcf8934b",
+    "url": "/static/js/main.24395b9a.chunk.js"
+  },
+  {
+    "revision": "b2d15a7841991329bd39",
+    "url": "/static/js/1.b2d15a78.chunk.js"
+  },
+  {
+    "revision": "24395b9ac66cfcf8934b",
+    "url": "/static/css/main.44ed56d6.chunk.css"
+  },
+  {
+    "revision": "f624ce64f0921237cd9fe136826998bd",
+    "url": "/index.html"
+  }
+];
